6d3e9bb59fdc00539736ec96813e17bb0703cd93
yangjie <746576029@qq.com>
2020-04-24 17:22:47 +0800

    提交

--- Application/Admin/Controller/Permission/UserController.class.php
+++ Application/Admin/Controller/Permission/UserController.class.php
@@ -1,10 +1,17 @@
 <?php
+
 namespace Admin\Controller\Permission;
+
 use Admin\Controller\BaseController;
 
-class UserController extends BaseController {
-    public function index(){
-        $this->assign('aaa','111');
+class UserController extends BaseController
+{
+    public function index()
+    {
+        $this->display();
+    }
+    public function add()
+    {
         $this->display();
     }
 }
