332707b87f3d4465b09fd6043f7357cb624b1faa
yangjie <746576029@qq.com>
2020-04-26 22:05:49 +0800

    提交

--- Application/Admin/Controller/Permission/AuthController.class.php
+++ Application/Admin/Controller/Permission/AuthController.class.php
@@ -1,9 +1,45 @@
 <?php
+
 namespace Admin\Controller\Permission;
+
 use Admin\Controller\BaseController;
 
-class AuthController extends BaseController {
-    public function index(){
+class AuthController extends BaseController
+{
+    public function index()
+    {
+        $authList = M('auth')->field('id,auth_name,status,rule,parent_id,create_time,update_time')->select();
+        $list = list_to_tree($authList);
+        $this->assign('list', $list);
         $this->display();
+    }
+
+    public function add()
+    {
+        if(IS_POST){
+            $data['auth_name']=I('post.auth_name');
+            $data['rule']=I('post.rule');
+            $data['status']=I('post.status');
+            $data['parent_id']=I('post.parent_id');
+            $re=M('auth')->add($data);
+            if($re){
+                $this->success('新增成功', U('admin/permission/auth/index'));
+            }else{
+                $this->error('新增失败');
+            }
+        }else{
+            $authList = M('auth')->field('id,auth_name,status,rule,parent_id,create_time,update_time')->select();
+            $list = list_to_tree($authList);
+            $this->assign('list', $list);
+            $this->display();
+        }
+    }
+
+    public function edit()
+    {
+    }
+
+    public function del()
+    {
     }
 }
--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -1,10 +1,61 @@
 <?php
-function pr($param){
+
+/**
+ * 调试输出
+ *
+ * @param array $param
+ */
+function pr($param)
+{
     echo '<pre>';
     print_r($param);
     echo '</pre>';
 }
-function prd($param){
+
+/**
+ * 调试输出
+ *
+ * @param array $param
+ */
+function prd($param)
+{
     pr($param);
     die;
+}
+
+/**
+ * @param array $list 要转换的结果集
+ * @param string $parent_id parent标记字段
+ * @param string $child 子集合键名
+ * @param int $root 顶级parent_id的值
+ */
+function list_to_tree($list, $pk = 'id', $pid = 'parent_id', $child = 'children', $root = 0)
+{
+    //创建Tree
+    $tree = array();
+
+    if (is_array($list)) {
+        //创建基于主键的数组引用
+        $refer = array();
+
+        foreach ($list as $key => $data) {
+            $refer[$data[$pk]] = &$list[$key];
+        }
+
+        foreach ($list as $key => $data) {
+            //判断是否存在parent
+            $parantId = $data[$pid];
+
+            if ($root == $parantId) {
+                $tree[] = &$list[$key];
+            } else {
+                if (isset($refer[$parantId])) {
+                    $parent = &$refer[$parantId];
+                    $parent[$child][] = &$list[$key];
+                }
+            }
+        }
+    }
+
+    return $tree;
 }
