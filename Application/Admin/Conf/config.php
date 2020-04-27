b85105b3e32524f9a691f106b96dbada6dda6c00
yangjie <746576029@qq.com>
2020-04-27 16:03:47 +0800

    提交

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-	'CONTROLLER_LEVEL'      =>  2,//多级控制器
+	'CONTROLLER_LEVEL'      =>  2,//多级控制器，开启多级控制器后原路由配置失效
 	'LAYOUT_ON'				=>  true,// 是否开启layout布局 默认关闭
 	'TOKEN_ON'              =>  true,// 是否开启令牌验证 默认关闭
 );
--- Application/Admin/Controller/EmptyController.class.php
+++ Application/Admin/Controller/EmptyController.class.php
@@ -3,7 +3,12 @@ namespace Admin\Controller;
 use Think\Controller;
 class EmptyController extends Controller{
     public function index(){
+        if(C('CONTROLLER_LEVEL')>1){
+            if(strpos(CONTROLLER_NAME,'/') === false){
+                $this->redirect('admin/'.CONTROLLER_NAME.'/index');
+            }
+        }
         //处理空控制器
-        $this->display('/404');
+        $this->error('非法操作');
     }
 }
